346f9c6688400da59a4cf418a4bb8b89cf998a41
Charlie <ccariste@gmail.com>
2018-03-27 19:27:21 -0700

    CRC: Built component files and imported dummy data

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PostContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        console.log(this.props.post);
+        return (
+            <h1></h1>
+
+
+
+        )
+    }
+
+}
+
+export default PostContainer;
