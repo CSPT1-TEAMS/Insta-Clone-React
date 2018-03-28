287b4e02ffadf5b1fe2c6021e7611fa5f5c9925b
Bhavik Ravani <ravanibhavik@gmail.com>
2018-03-27 23:25:07 -0400

    Updated App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import { PostContainer } from './components/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({data: dummyData});
+  }
+
   render() {
+    console.log(this.state.data);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.data.map(post => {
+            <PostContainer post={ post } />
+          })
+        }
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PostContainer = (props) => {
+    <Comment comments={props.comments} /> 
+}
